510fa1ecf18eac4db40f0a0ca2ae1368757d2423
rasonyudha4 <rasonyudha921@gmail.com>
2024-10-22 13:30:15 +0700

    Adding models and migrations
    - Add material model
    - Add Order model
    - Add Order Material model
    - Add migration for materials, orders, and orderMaterials table

---
+++ database/migrations/2024_10_22_062323_create_ordermaterial_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ordermaterial', function (Blueprint $table) {
+            $table->foreign('materialId')->references('id')->on('materials')->onDelete('cascade');
+            $table->foreign('Work_order_number')->references('Work_order_number')->on('order')->onDelete('cascade');
+            $table->integer('Percentage');
+            $table->date('Finished_Date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ordermaterial');
+    }
+};
