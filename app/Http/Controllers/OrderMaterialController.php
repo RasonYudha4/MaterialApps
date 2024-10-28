b872df115f50d804a4b19ed75aeb528eab29dfbf
rasonyudha4 <rasonyudha921@gmail.com>
2024-10-28 07:17:09 +0700

    Project Done

--- app/Http/Controllers/OrderMaterialController.php
+++ app/Http/Controllers/OrderMaterialController.php
@@ -43,52 +43,12 @@ class OrderMaterialController extends Controller
     public function store(Request $request)
     {
 
-        $materialId = $request->input('material_id');
-        $inputPercentage = $request->input('percentage');
-        $orderId = $request->input('order_id');
-
         // Fetch all orders
         $workOrders = OrderMaterial::all();
-
-        $order = Order::find($orderId);
-        $material = Material::find($materialId);
-
-        // Get Work_date from the order
-        $workDate = $order->Work_date;
-
-        // Get days from the material
-        $daysToAdd = $material->days;
-
-        // Calculate Finished_date
-        $newDate = new \DateTime($workDate);
-        $newDate->modify("+$daysToAdd days");
-        $finishedDate = $newDate->format('Y-m-d');
+        $materials = Material::all();
 
         // Initialize sumPercentage as an associative array
         $sumPercentage = [];
-
-        // foreach($Orders as $order) {
-        //     $orderId = $order->Work_order_number;
-        //     // Calculate the current sum of percentages for each orderId
-        //     foreach ($workOrders as $workOrder) {
-        //         $workorderId = $workOrder->orderId;
-    
-        //         if (!isset($sumPercentage[$workorderId])) {
-        //             $sumPercentage[$workorderId] = 0; // Initialize it to 0 if it doesn't exist
-        //         }
-    
-        //         // Accumulate the percentage for each orderId
-        //         $sumPercentage[$workorderId] += $workOrder->Percentage;
-
-        //         if ($orderId = $workorderId) {
-        //             $materialDate = Material::find($materialId);
-        //             $startDate = $order->Work_date;
-        //             $newDate = new \DateTime($startDate);
-        //             $newDate->modify("+$materialDate days");
-        //             $finishedDate = $newDate->format('Y-m-d');
-        //         }
-        //     }
-        // }
 
         // Calculate the current sum of percentages for each orderId
         foreach ($workOrders as $workOrder) {
@@ -101,15 +61,36 @@ class OrderMaterialController extends Controller
             // Accumulate the percentage for each orderId
             $sumPercentage[$orderId] += $workOrder->Percentage;
         }
+
+        $inputPercentage = $request->input('percentage');
+        $orderId = $request->input('order_id');
+
+        if (isset($sumPercentage[$orderId]) && ($sumPercentage[$orderId] + $inputPercentage > 100)) {
+            return redirect()->back()->withErrors(['percentage' => 'The total percentage for this order cannot exceed 100.']);
+        }
+
+        $sumDays = 0;
+
+        foreach ($materials as $material) {
+            $sumDays += $material->days;
+        }
+
+        $order = Order::find($orderId);
+
+        // Get Work_date from the order
+        $workDate = $order->Work_date;
+
+        // Calculate Finished_date
+        $newDate = new \DateTime($workDate);
+        $newDate->modify("+$sumDays days");
+        $finishedDate = $newDate->format('Y-m-d');
+
+        dd($sumPercentage);
+
         $request->validate([
             'material_id' => 'required',
             'percentage' => 'required|integer|min:1|max:100',
         ]);
-
-        if (isset($sumPercentage[$orderId]) && ($sumPercentage[$orderId] + $inputPercentage > 100)) {
-            // Show an error message (you can use session flash data or return a response)
-            return redirect()->back()->withErrors(['percentage' => 'The total percentage for this order cannot exceed 100.']);
-        }
 
         OrderMaterial::create([
             'orderId' => $orderId,
